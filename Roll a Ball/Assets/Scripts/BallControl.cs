9277e00c6639f074b9b2a1acb1e7f6c4272f29dc
BDogukanYildirim <146746412+BDogukanYildirim@users.noreply.github.com>
2024-07-08 20:42:22 +0300

    proje oluşturuldu

---
+++ Roll a Ball/Assets/Scripts/BallControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControl : MonoBehaviour
+{
+    public int hiz;
+    public int skor;
+    public int crystals;
+    public Text pointText;
+    public Text finishText;
+    public Button exitButton;
+
+
+    public Rigidbody fizik;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float dikey = Input.GetAxisRaw("Vertical");
+        float yatay = Input.GetAxisRaw("Horizontal");
+
+        Vector3 vektor = new Vector3 ( yatay , 0 , dikey );
+
+        fizik.AddForce(vektor * hiz);
+
+           
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+        skor++;
+        pointText.text = "Point : " + skor;
+
+        if (skor == crystals)
+        {
+            finishText.gameObject.SetActive(true);
+            exitButton.gameObject.SetActive(true);
+        }
+
+    }
+}
---
+++ Roll a Ball/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject ball;
+    Vector3 distence;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distence = transform.position - ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = ball.transform.position + distence;
+    }
+}
